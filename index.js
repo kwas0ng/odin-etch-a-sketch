cbaf74f8923e4fba75af025168b5e337f8cdfb87
james <jamesdeleon373@gmail.com>
2023-03-29 20:59:35 +0800

    Added click + drag functionality to sketch and included documentation

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-const container = document.getElementById("container");
+const sketchArea = document.getElementById("sketchArea");
 
 let rows = document.getElementsByClassName("gridRow");
 
@@ -7,11 +7,18 @@ let cells = document.getElementsByClassName("cell");
 
 
 
+/*
+
+    Make it so that the size of the grid can be dynamic
+*/
+
+
+
 for (let i = 0; i < 16; i++){
 
     let row = document.createElement("div");
 
-    container.appendChild(row).className = "gridRow";
+    sketchArea.appendChild(row).className = "gridRow";
 }
 
 
@@ -25,19 +32,59 @@ for (let i = 0 ; i < 16; i++){
     }
 }
 
-const elements = document.querySelectorAll("div.cell");
-console.log(elements)
 
-elements.forEach((item) => {
-    item.addEventListener('mousemove', () => {
+
+let isDrawing = false;
+
+
+
+const squares = document.querySelectorAll("div.cell");
+console.log(squares)
+
+
+/*
+    The squares or each cell of the grid will be added an event listener.
+*/
+squares.forEach((square) => {
+
+    /*
+
+    Once the user triggers a mousedown event on a cell, 
+    isDrawing will be set to true and the user will be able to drag and color each cells 
+    (this is achieved by adding the mouseover listener).
+
+    A mouseup listener (or when the user releases the mouse click), isDrawing will be then set to false
+    restricting the user to color each cells to avoid mistakes.
+    
+    */
+
+
+    square.addEventListener('mousedown', (e) => {
         
-        item.setAttribute('style', 'background-color:black');
+        isDrawing = true;
+        
+        square.setAttribute('style', 'background-color:black');   
+        
     })
 
-    // item.addEventListener('touchend', () => {
-    //     item.setAttribute('style', 'background-color:black');
-    // })
-    
-    
+    square.addEventListener('mouseup', (e) => {
+        isDrawing = false;
+    })
+
+
+    square.addEventListener('mouseover', (e) => {
+
+        if(isDrawing) {
+            square.setAttribute('style', 'background-color:black');
+        }
+    })
+
+
+})
+
+
+ // if the mouse cursor leaves the sketch area, drawing will be set to false
+sketchArea.addEventListener('mouseleave', (e) => {
+    isDrawing = false;
 })
 
